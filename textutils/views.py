46831626d2e00b7106966dcd95f1a8b67e939cdb
SmartyPants <saikap2009@gmail.comRun>
2022-08-14 19:37:04 +0200

    template home

--- textutils/views.py
+++ textutils/views.py
@@ -2,27 +2,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
 
-
 def index(request):
-    info = {'name':'Siddhant & Soubhagya', 'place':'Unknown Galaxy'}
-    return  render(request, 'index.html', info)
-
+    return  render(request, 'index.html')
 
 def removepunc(request):
+    text_web = request.GET.get('text', 'default')
+    print(text_web)
     return  HttpResponse("Remove Puctuation")
-
 
 def capfirst(request):
     return  HttpResponse("Capitalise First")
 
-
 def newlineremove(request):
     return  HttpResponse("New Line Remover")
 
-
 def spaceremove(request):
     return  HttpResponse("Space Remover")
-
 
 def charcount(request):
     return  HttpResponse("Character Count")
