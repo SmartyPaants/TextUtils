cb6f2809f1f97eb770fe5967fe8017eeb6626e3b
SmartyPants <saikap2009@gmail.comRun>
2022-08-22 19:06:21 +0200

    logic

--- textutils/views.py
+++ textutils/views.py
@@ -5,19 +5,37 @@ from django.shortcuts import render
 def index(request):
     return  render(request, 'index.html')
 
-def analyse(request):
+def analyse(request):                                       # Function name
     # Get the text
     text_web = request.GET.get('text', 'default')
-    removepunc=request.GET.get('removepunc','off')
 
-    if removepunc == "on":
-        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~+=¬`|'''
-        analysed = ""
-        for char in text_web:
-            if char not in punctuations:
-                analysed = analysed + char
-        params = {'purpose': 'Removed Punctuations', 'analysed_text': analysed}
-        return render(request, 'analyse.html', params)
+    # Remove Punctuation
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~+=¬`|'''              # All punctuations
+    removed_punc = ""
+    for char in text_web:
+        if char not in punctuations:
+            removed_punc = removed_punc + char
 
-    else:
-        return HttpResponse('Error')
+    # UPPERCASE
+    UPPERCASE = ""
+    for char in text_web:
+        UPPERCASE = UPPERCASE + char.upper()
+
+    # lowercase
+    lowercase = ""
+    for char in text_web:
+        lowercase = lowercase + char.lower()
+
+    # Character Count
+    charcount = len(text_web)
+
+    # Word Count
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~+=¬`|'''              # All punctuations
+    text = text_web.strip()
+    count = 1
+    for i in text:
+        if i == " " or i == punctuations:
+            count = count + 1
+            wordcount = count
+    params = {'wordcount': wordcount, 'og_text': text_web, 'remove_punc': removed_punc, 'UPPERCASE': UPPERCASE, 'lowercase': lowercase, 'charcount': charcount}      # Parameters
+    return render(request, 'analyse.html', params)        # Sending information to 'analyse.html
